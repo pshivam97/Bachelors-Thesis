8282aae0bae1b836aac75f621ffb3ff4e370efa8
Shivam Pandey <37222952+pshivam97@users.noreply.github.com>
2020-05-21 01:05:21 +0530

    Update

--- Research Work/My Work/MOEAs/MyNSGA-2_modified.py
+++ Research Work/My Work/MOEAs/MyNSGA-2_modified.py
@@ -1,10 +1,3 @@
-# https://github.com/haris989/NSGA-II
-
-# Program Name: NSGA-II.py
-# Description: This is a python implementation of Prof. Kalyanmoy Deb's popular NSGA-II algorithm
-# Author: Haris Ali Khan
-# Supervisor: Prof. Manoj Kumar Tiwari
-
 #Importing required modules
 import math
 import random
@@ -22,22 +15,22 @@ def function2(x):
     return value
 
 #Function to find index of list
-def index_of(a,list): #returns index of "a" in "list" , else "-1"
-    for i in range(0,len(list)):
-        if list[i] == a:
+def index_of(a,List): #returns index of "a" in "list" , else "-1"
+    for i in range(len(List)):
+        if List[i] == a:
             return i
     return -1
 
 #Function to carry out NSGA-II's fast non dominated sort
 def fast_non_dominated_sort(values1, values2, rank):
-    S=[[] for i in range(0,len(values1))]
+    S=[[] for i in range(len(values1))]
     front = [[]]
-    n = [0 for i in range(0,len(values1))]
+    n = [0 for i in range(len(values1))]
 
-    for p in range(0,len(values1)):
+    for p in range(len(values1)):
         S[p]=[]
         n[p]=0
-        for q in range(0, len(values1)):
+        for q in range(len(values1)):
             if (values1[p] < values1[q] and values2[p] < values2[q]) or (values1[p] <= values1[q] and values2[p] < values2[q]) or (values1[p] < values1[q] and values2[p] <= values2[q]):
                 if q not in S[p]:
                     S[p].append(q)
@@ -50,12 +43,12 @@ def fast_non_dominated_sort(values1, values2, rank):
                 front[0].append(p)
 
     i = 0
-    while(front[i] != []):
+    while front[i] != [] :
         Q = []
         for p in front[i]:
             for q in S[p]:
                 n[q] = n[q] - 1
-                if (n[q] == 0):
+                if n[q] == 0 :
                     rank[q] = i + 1
                     if q not in Q:
                         Q.append(q)
@@ -68,27 +61,40 @@ def fast_non_dominated_sort(values1, values2, rank):
 #Function to sort by values
 def sort_by_values(front, values):
     sorted_list = []
-    while(len(sorted_list) != len(front)):
+    while len(sorted_list) != len(front) :
         sorted_list.append(index_of(min(values),values))
         values[index_of(min(values),values)] = math.inf
     return sorted_list
 
 #Function to calculate crowding distance
-def crowding_distance(values1, values2, values3,front):
-    distance = [0 for i in range(0,len(front))]
-    sorted1 = sort_by_values(front, values1[:])
-    sorted2 = sort_by_values(front, values2[:])
-    sorted3 = sort_by_values(front, values3[:])
+def crowding_distance(values1, values2, front):
+    distance = [0 for i in range(len(front))]
+    front_values1 = []
+    front_values2 = []
 
-    distance[0] = 4444444444444444  ### BIG NUMBER
-    distance[len(front) - 1] = 4444444444444444  ### BIG NUMBER
+    for i in front :
+        front_values1.append(values1[i])
+        front_values2.append(values2[i])
 
-    for k in range(1,len(front)-1):
-        distance[k] = distance[k] + (values1[sorted1[k+1]] - values1[sorted1[k-1]])/(max(values1)-min(values1) + 1)
-    for k in range(1,len(front)-1):
-        distance[len(front)-k-1] = distance[len(front)-k-1] + (values2[sorted2[k+1]] - values2[sorted2[k-1]])/(max(values2)-min(values2) + 1)
+    sorted1 = sort_by_values(front, front_values1[:])
+    sorted2 = sort_by_values(front, front_values2[:])
+    # sorted3 = sort_by_values(front, values3[:])
+
+    distance1 = [0 for i in range(len(front))]
+    distance2 = [0 for i in range(len(front))]
+    distance1[sorted1[0]] = 4444444444444444  ### BIG NUMBER
+    distance2[sorted2[0]] = 4444444444444444  ### BIG NUMBER
+
+
     for k in range(1,len(front)-1) :
-        distance[k] = distance[k] + (values3[sorted3[k+1]] - values3[sorted3[k-1]])/(max(values3) - min(values3))
+        distance1[sorted1[k]] = (front_values1[sorted1[k+1]] - front_values1[sorted1[k-1]])/(max(front_values1)-min(front_values1) + 1)
+        distance2[sorted2[k]] = (front_values2[sorted2[k+1]] - front_values2[sorted2[k-1]])/(max(front_values2)-min(front_values2) + 1)
+
+    for k in range(len(front)):
+        distance[k] = distance1[k] + distance2[k]
+
+    # for k in range(1,len(front)-1) :
+    #     distance[k] = distance[k] + (values3[sorted3[k+1]] - values3[sorted3[k-1]])/(max(values3) - min(values3))
 
     return distance
 
@@ -168,31 +174,29 @@ if __name__ == '__main__' :
 
     #######################    Main program starts here   #########################
     pop_size = 40
-    max_gen = 200
+    max_gen = 100
     mutationRate = 0.2
-    rank = [0 for i in range(0,pop_size)]
+    rank = [0 for i in range(pop_size)]
 
     #Initialization
-    min_x = -100
-    max_x = 100
-    population = [min_x + (max_x-min_x)*random.random() for i in range(0,pop_size)]
+    min_x = -1000
+    max_x = 1000
+    population = [min_x + (max_x-min_x)*random.random() for i in range(pop_size)]
     P_t = copy.deepcopy(population) ## Population list initialized of size = pop_size
 
-    function1_values = [function1(population[i]) for i in range(0,pop_size)]
-    function2_values = [function2(population[i]) for i in range(0,pop_size)]
+    function1_values = [function1(population[i]) for i in range(pop_size)]
+    function2_values = [function2(population[i]) for i in range(pop_size)]
 
     if max_gen == 0 :
-        print("FINAL SOLUTION : \n",population,"\n\n",max(population),"  ",min(population))
-        function1 = [i for i in function1_values]
-        function2 = [j for j in function2_values]
+        print("FINAL SOLUTION : \n",population,"\n\nMax Data Point: ",max(population)," \nMin Data Point: ",min(population))
         plt.xlabel('x^2', fontsize=15)
         plt.ylabel('(x-2)^2', fontsize=15)
-        plt.scatter(function1, function2)
+        plt.scatter(function1_values, function2_values)
         plt.grid()
         plt.show()
         exit()
 
-    fronts = fast_non_dominated_sort(function1_values[:],function2_values[:],rank)
+    fronts = fast_non_dominated_sort(function1_values[:], function2_values[:], rank)
     crowding_distance_values = []
     previous_R_t = None
 
@@ -210,23 +214,23 @@ if __name__ == '__main__' :
     R_t = population
 
     generation_no = 0
-    no_of_generations = []
-    average_crowding_distance_objective_space = []
-    average_crowding_distance_data_space = []
+    # no_of_generations = []
+    # average_crowding_distance_objective_space = []
+    # average_crowding_distance_data_space = []
 
     while generation_no < max_gen :
 
         # variable_space = copy.deepcopy(R_t)
-        function1_values = [function1(R_t[i])for i in range(0,2*pop_size)]
-        function2_values = [function2(R_t[i])for i in range(0,2*pop_size)]
+        function1_values = [function1(R_t[i]) for i in range(2*pop_size)]
+        function2_values = [function2(R_t[i]) for i in range(2*pop_size)]
 
-        rank = [0 for i in range(0,2*pop_size)]
+        rank = [0 for i in range(2*pop_size)]
 
         fronts = fast_non_dominated_sort(function1_values[:],function2_values[:],rank)
 
         del crowding_distance_values[:]
 
-        for i in range(0,len(fronts)):
+        for i in range(len(fronts)):
             crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],fronts[i][:]))
 
         new_solution = []
@@ -234,6 +238,7 @@ if __name__ == '__main__' :
             if (pop_size - len(new_solution)) >= len(fronts[i]) : ## If the current front can be fully accomodated in new generation
                 for each_value in fronts[i] :
                     new_solution.append(each_value)
+
             elif (len(new_solution) == pop_size):
                 break
 
@@ -267,58 +272,55 @@ if __name__ == '__main__' :
 
         generation_no += 1
 
-        sum_in_objective_space = 0
-        for i in range(len(P_t)) :
-            r = index_of(P_t[i],previous_R_t)
-            sum_in_objective_space += crowding_distance_values[rank[r]][index_of(r,fronts[rank[r]])]
+        # sum_in_objective_space = 0
+        # for i in range(len(P_t)) :
+        #     r = index_of(P_t[i],previous_R_t)
+        #     sum_in_objective_space += crowding_distance_values[rank[r]][index_of(r,fronts[rank[r]])]
 
-        average_crowding_distance_objective_space.append(sum_in_objective_space/pop_size)
+        # average_crowding_distance_objective_space.append(sum_in_objective_space/pop_size)
 
-        sum_in_data_space = 0
-        P_t_copy = copy.deepcopy(P_t)
-        P_t_copy.sort()
-        crowding_distance_data_space = [0 for i in range(len(P_t_copy))]
-        crowding_distance_data_space[0] = 4444444444444444
-        crowding_distance_data_space[len(P_t_copy)-1] = 4444444444444444
-        P_t_max = max(P_t_copy)
-        P_t_min = min(P_t_copy)
+        # sum_in_data_space = 0
+        # P_t_copy = copy.deepcopy(P_t)
+        # P_t_copy.sort()
+        # crowding_distance_data_space = [0 for i in range(len(P_t_copy))]
+        # crowding_distance_data_space[0] = 4444444444444444
+        # crowding_distance_data_space[len(P_t_copy)-1] = 4444444444444444
+        # P_t_max = max(P_t_copy)
+        # P_t_min = min(P_t_copy)
 
-        for i in range(1,len(P_t)-1) :
-            crowding_distance_data_space[i] += (abs(P_t_copy[i+1] - P_t_copy[i]) + abs(P_t_copy[i] - P_t_copy[i-1]))/(P_t_max - P_t_min + 1)
-            sum_in_data_space += crowding_distance_data_space[i]
+        # for i in range(1,len(P_t)-1) :
+        #     crowding_distance_data_space[i] += (abs(P_t_copy[i+1] - P_t_copy[i]) + abs(P_t_copy[i] - P_t_copy[i-1]))/(P_t_max - P_t_min + 1)
+        #     sum_in_data_space += crowding_distance_data_space[i]
 
-        average_crowding_distance_data_space.append(sum_in_data_space/pop_size-2)
-        no_of_generations.append(generation_no)
+        # average_crowding_distance_data_space.append(sum_in_data_space/pop_size-2)
+        # no_of_generations.append(generation_no)
 
         ### WHILE LOOP ENDS HERE
 
     population = P_t
-    print("FINAL SOLUTION : \n",population,"\n\n",max(population),"  ",min(population))
+    # print("FINAL SOLUTION : \n",population,"\n\n",max(population),"  ",min(population))
 
     function1_values = [function1(population[i]) for i in range(0,pop_size)]
     function2_values = [function2(population[i]) for i in range(0,pop_size)]
-    #Lets plot the final front now
-    function1 = [i for i in function1_values]
-    function2 = [j for j in function2_values]
 
     f1 = plt.figure(1)
     plt.xlabel('x^2', fontsize=15)
     plt.ylabel('(x-2)^2', fontsize=15)
-    plt.scatter(function1, function2)
+    plt.scatter(function1_values, function2_values)
     plt.grid()
+    plt.savefig("O.png")
 
     f2 = plt.figure(figsize=(6.4,2))
     plt.plot(population,[0 for i in range(pop_size)],'x')
     plt.grid()
+    plt.savefig("D.png")
 
-    f3 = plt.figure(figsize=(12,7))
-    ax = f3.add_subplot(111)
+    # f3 = plt.figure(figsize=(12,7))
+    # ax = f3.add_subplot(111)
 
-    plt.xlabel('No. of Generations (Computational Effort)', fontsize=15)
-    plt.ylabel('Average Crowding Distance', fontsize=15)
-    ax.plot(no_of_generations,average_crowding_distance_objective_space,color='blue',marker='o',markersize=4,linewidth=1)
-    ax.plot(no_of_generations,average_crowding_distance_data_space,color='orange',marker='o',markersize=4,linewidth=1)
-    plt.grid()
-
-    plt.show()
-    exit()
+    # plt.xlabel('No. of Generations (Computational Effort)', fontsize=15)
+    # plt.ylabel('Average Crowding Distance', fontsize=15)
+    # ax.plot(no_of_generations,average_crowding_distance_objective_space,color='blue',marker='o',markersize=4,linewidth=1)
+    # ax.plot(no_of_generations,average_crowding_distance_data_space,color='orange',marker='o',markersize=4,linewidth=1)
+    # plt.grid()
+    # plt.show()
