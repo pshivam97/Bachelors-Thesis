bd38379ae857631e29ba7f2c1eb7e538fa423816
Shivam Pandey <37222952+pshivam97@users.noreply.github.com>
2020-05-25 11:56:50 +0530

    Update MyNSGA-2_modified.py

--- Research Work/My Work/MOEAs/MyNSGA-2_modified.py
+++ Research Work/My Work/MOEAs/MyNSGA-2_modified.py
@@ -15,7 +15,7 @@ def function2(x):
     return value
 
 #Function to find index of list
-def index_of(a,List): #returns index of "a" in "list" , else "-1"
+def index_of(a,List): #returns index of "a" in "list", else "-1"
     for i in range(len(List)):
         if List[i] == a:
             return i
@@ -67,18 +67,20 @@ def sort_by_values(front, values):
     return sorted_list
 
 #Function to calculate crowding distance
-def crowding_distance(values1, values2, front):
+def crowding_distance(Obj_values1, Obj_values2, data_space, front):
     distance = [0 for i in range(len(front))]
+
+    # Calculating crowding distance in Objective Space
+
     front_values1 = []
     front_values2 = []
 
     for i in front :
-        front_values1.append(values1[i])
-        front_values2.append(values2[i])
+        front_values1.append(Obj_values1[i])
+        front_values2.append(Obj_values2[i])
 
     sorted1 = sort_by_values(front, front_values1[:])
     sorted2 = sort_by_values(front, front_values2[:])
-    # sorted3 = sort_by_values(front, values3[:])
 
     distance1 = [0 for i in range(len(front))]
     distance2 = [0 for i in range(len(front))]
@@ -93,8 +95,26 @@ def crowding_distance(values1, values2, front):
     for k in range(len(front)):
         distance[k] = distance1[k] + distance2[k]
 
+    # Calculating crowding distance in Data Space
+
+    # data_values = []
+    # for i in front :
+    #     data_values.append(data_space[i])
+
+    # sorted3 = sort_by_values(front, data_values[:])
+    # distance3 = [0 for i in range(len(front))]
+    # distance3[sorted3[0]] = distance3[sorted3[len(front)-1]] = 4444444444444444
+
     # for k in range(1,len(front)-1) :
-    #     distance[k] = distance[k] + (values3[sorted3[k+1]] - values3[sorted3[k-1]])/(max(values3) - min(values3))
+    #     distance3[sorted3[k]] = (data_values[sorted3[k+1]] - data_values[sorted3[k-1]])/(max(data_values) - min(data_values) + 1)
+
+    # for k in range(len(front)):
+        # distance[k] = distance3[k]
+
+    # Calculating the crowding distance in both Objective and Data Space
+
+    # for k in range(len(front)):
+    #     distance[k] = distance1[k] + distance2[k] + distance3[k]
 
     return distance
 
@@ -106,12 +126,12 @@ def crossover_and_mutation(p1,p2): # p1 is parent1 (real number) | p2 is parent2
     # beta = 0
     # n = 3
     # r = random.random()
-    #
+
     # if r <= 0.5 :
     #     beta = (2*r)**(1/(n+1))
     # else :
     #     beta = (1/(2*(1-r)))**(1/(n+1))
-    #
+
     # child1 = 0.5*((1+beta)*p1 + (1-beta)*p2) # child1
     # child2 = 0.5*((1-beta)*p1 + (1+beta)*p2) # child2
 
@@ -148,7 +168,7 @@ def binary_tournament_selection(fronts,previous_pool,current_pool,crowding_value
         else :
             crowding_distances = [crowding_values[rank[r1]][index_of(r1,fronts[rank[r1]])], crowding_values[rank[r2]][index_of(r2,fronts[rank[r2]])]]
             max_crowding_dist = max(crowding_distances)
-            i = index_of(max_crowding_dist,crowding_distances)
+            i = index_of(max_crowding_dist, crowding_distances)
 
             if i == 0:
                 return p1
@@ -174,132 +194,160 @@ if __name__ == '__main__' :
 
     #######################    Main program starts here   #########################
     pop_size = 40
-    max_gen = 100
+    max_gen = 200
     mutationRate = 0.2
     rank = [0 for i in range(pop_size)]
-
-    #Initialization
+    run_of_algorithm = 5
     min_x = -1000
     max_x = 1000
-    population = [min_x + (max_x-min_x)*random.random() for i in range(pop_size)]
-    P_t = copy.deepcopy(population) ## Population list initialized of size = pop_size
+    average_diversity_metric_objective_space = 0
+    average_diversity_metric_data_space = 0
+    d_l = 0
+    d_f = 0
 
-    function1_values = [function1(population[i]) for i in range(pop_size)]
-    function2_values = [function2(population[i]) for i in range(pop_size)]
+    for i in range(run_of_algorithm) :
 
-    if max_gen == 0 :
-        print("FINAL SOLUTION : \n",population,"\n\nMax Data Point: ",max(population)," \nMin Data Point: ",min(population))
-        plt.xlabel('x^2', fontsize=15)
-        plt.ylabel('(x-2)^2', fontsize=15)
-        plt.scatter(function1_values, function2_values)
-        plt.grid()
-        plt.show()
-        exit()
+        #Initialization
+        population = [min_x + (max_x-min_x)*random.random() for i in range(pop_size)]
+        P_t = copy.deepcopy(population) ## Population list initialized of size = pop_size
 
-    fronts = fast_non_dominated_sort(function1_values[:], function2_values[:], rank)
-    crowding_distance_values = []
-    previous_R_t = None
+        function1_values = [function1(population[i]) for i in range(pop_size)]
+        function2_values = [function2(population[i]) for i in range(pop_size)]
 
-    while len(population) != 2 * pop_size :
-        parent1 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,False)
-        parent2 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,False)
+        if max_gen == 0 :
+            print("FINAL SOLUTION : \n",population,"\n\nMax Data Point: ",max(population)," \nMin Data Point: ",min(population))
+            plt.xlabel('x^2', fontsize=15)
+            plt.ylabel('(x-2)^2', fontsize=15)
+            plt.scatter(function1_values, function2_values)
+            plt.grid()
+            plt.show()
+            exit()
 
-        (child1,child2) = crossover_and_mutation(P_t[parent1],P_t[parent2])
-        population.append(child1)
-        if len(population) == 2*pop_size :
-            break
-        else :
-            population.append(child2)
+        fronts = fast_non_dominated_sort(function1_values[:], function2_values[:], rank)
+        crowding_distance_values = []
+        previous_R_t = None
 
-    R_t = population
-
-    generation_no = 0
-    # no_of_generations = []
-    # average_crowding_distance_objective_space = []
-    # average_crowding_distance_data_space = []
-
-    while generation_no < max_gen :
-
-        # variable_space = copy.deepcopy(R_t)
-        function1_values = [function1(R_t[i]) for i in range(2*pop_size)]
-        function2_values = [function2(R_t[i]) for i in range(2*pop_size)]
-
-        rank = [0 for i in range(2*pop_size)]
-
-        fronts = fast_non_dominated_sort(function1_values[:],function2_values[:],rank)
-
-        del crowding_distance_values[:]
-
-        for i in range(len(fronts)):
-            crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],fronts[i][:]))
-
-        new_solution = []
-        for i in range(len(fronts)) :
-            if (pop_size - len(new_solution)) >= len(fronts[i]) : ## If the current front can be fully accomodated in new generation
-                for each_value in fronts[i] :
-                    new_solution.append(each_value)
-
-            elif (len(new_solution) == pop_size):
-                break
-
-            else : ## If the current front cannot be fully accomodated in the new generation, then we decide by crowding distance to preserve diversity
-                last_front_to_be_considered = [index_of(fronts[i][j],fronts[i]) for j in range(0,len(fronts[i]))]
-                front_sorted = sort_by_values(last_front_to_be_considered, crowding_distance_values[i][:])
-                front = [fronts[i][front_sorted[j]] for j in range(0,len(fronts[i]))]
-                front.reverse()
-
-                for each_value in front:
-                    if len(new_solution) != pop_size :
-                        new_solution.append(each_value)
-                    else :
-                        break
-
-        previous_R_t = R_t
-        P_t = [R_t[i] for i in new_solution]
-        R_t = copy.deepcopy(P_t)
-
-        ### Now, we develop the Q_t to get new R_t
-        while len(R_t) != 2 * pop_size :
-            parent1 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,True)
-            parent2 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,True)
+        while len(population) != 2 * pop_size :
+            parent1 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,False)
+            parent2 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,False)
 
             (child1,child2) = crossover_and_mutation(P_t[parent1],P_t[parent2])
-            R_t.append(child1)
-            if len(R_t) == 2*pop_size :
+            population.append(child1)
+            if len(population) == 2*pop_size :
                 break
             else :
-                R_t.append(child2)
+                population.append(child2)
 
-        generation_no += 1
+        R_t = population
 
-        # sum_in_objective_space = 0
-        # for i in range(len(P_t)) :
-        #     r = index_of(P_t[i],previous_R_t)
-        #     sum_in_objective_space += crowding_distance_values[rank[r]][index_of(r,fronts[rank[r]])]
+        generation_no = 0
+        # no_of_generations = []
+        # average_crowding_distance_objective_space = []
+        # average_crowding_distance_data_space = []
 
-        # average_crowding_distance_objective_space.append(sum_in_objective_space/pop_size)
+        while generation_no < max_gen :
 
-        # sum_in_data_space = 0
-        # P_t_copy = copy.deepcopy(P_t)
-        # P_t_copy.sort()
-        # crowding_distance_data_space = [0 for i in range(len(P_t_copy))]
-        # crowding_distance_data_space[0] = 4444444444444444
-        # crowding_distance_data_space[len(P_t_copy)-1] = 4444444444444444
-        # P_t_max = max(P_t_copy)
-        # P_t_min = min(P_t_copy)
+            variable_space = copy.deepcopy(R_t)
+            function1_values = [function1(R_t[i]) for i in range(2*pop_size)]
+            function2_values = [function2(R_t[i]) for i in range(2*pop_size)]
 
-        # for i in range(1,len(P_t)-1) :
-        #     crowding_distance_data_space[i] += (abs(P_t_copy[i+1] - P_t_copy[i]) + abs(P_t_copy[i] - P_t_copy[i-1]))/(P_t_max - P_t_min + 1)
-        #     sum_in_data_space += crowding_distance_data_space[i]
+            rank = [0 for i in range(2*pop_size)]
 
-        # average_crowding_distance_data_space.append(sum_in_data_space/pop_size-2)
-        # no_of_generations.append(generation_no)
+            fronts = fast_non_dominated_sort(function1_values[:],function2_values[:],rank)
 
-        ### WHILE LOOP ENDS HERE
+            del crowding_distance_values[:]
 
-    population = P_t
-    # print("FINAL SOLUTION : \n",population,"\n\n",max(population),"  ",min(population))
+            for i in range(len(fronts)) :
+                crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],variable_space,fronts[i][:]))
 
+            new_solution = []
+            for i in range(len(fronts)) :
+                if (pop_size - len(new_solution)) >= len(fronts[i]) : ## If the current front can be fully accomodated in new generation
+                    for each_value in fronts[i] :
+                        new_solution.append(each_value)
+
+                elif (len(new_solution) == pop_size):
+                    break
+
+                else : ## If the current front cannot be fully accomodated in the new generation, then we decide by crowding distance to preserve diversity
+                    last_front_to_be_considered = [index_of(fronts[i][j],fronts[i]) for j in range(0,len(fronts[i]))]
+                    front_sorted = sort_by_values(last_front_to_be_considered, crowding_distance_values[i][:])
+                    front = [fronts[i][front_sorted[j]] for j in range(0,len(fronts[i]))]
+                    front.reverse()
+
+                    for each_value in front:
+                        if len(new_solution) != pop_size :
+                            new_solution.append(each_value)
+                        else :
+                            break
+
+            previous_R_t = R_t
+            P_t = [R_t[i] for i in new_solution]
+            R_t = copy.deepcopy(P_t)
+
+            ### Now, we develop the Q_t to get new R_t
+            while len(R_t) != 2 * pop_size :
+                parent1 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,True)
+                parent2 = binary_tournament_selection(fronts,previous_R_t,P_t,crowding_distance_values,rank,True)
+
+                (child1,child2) = crossover_and_mutation(P_t[parent1],P_t[parent2])
+                R_t.append(child1)
+                if len(R_t) == 2*pop_size :
+                    break
+                else :
+                    R_t.append(child2)
+
+            generation_no += 1
+
+            # sum_in_objective_space = 0
+            # for i in range(len(P_t)) :
+            #     r = index_of(P_t[i],previous_R_t)
+            #     sum_in_objective_space += crowding_distance_values[rank[r]][index_of(r,fronts[rank[r]])]
+
+            # average_crowding_distance_objective_space.append(sum_in_objective_space/pop_size)
+
+            # sum_in_data_space = 0
+            # P_t_copy = copy.deepcopy(P_t)
+            # P_t_copy.sort()
+            # crowding_distance_data_space = [0 for i in range(len(P_t_copy))]
+            # crowding_distance_data_space[0] = 4444444444444444
+            # crowding_distance_data_space[len(P_t_copy)-1] = 4444444444444444
+            # P_t_max = max(P_t_copy)
+            # P_t_min = min(P_t_copy)
+
+            # for i in range(1,len(P_t)-1) :
+            #     crowding_distance_data_space[i] += (abs(P_t_copy[i+1] - P_t_copy[i]) + abs(P_t_copy[i] - P_t_copy[i-1]))/(P_t_max - P_t_min + 1)
+            #     sum_in_data_space += crowding_distance_data_space[i]
+
+            # average_crowding_distance_data_space.append(sum_in_data_space/pop_size - 2)
+            # no_of_generations.append(generation_no)
+
+            ### WHILE LOOP ENDS HERE
+
+        population = P_t
+        function1_values = [function1(population[i]) for i in range(pop_size)]
+        function2_values = [function2(population[i]) for i in range(pop_size)]
+        sorted_list = sort_by_values(population,function1_values[:])
+        d_i = []
+
+        for i in range(pop_size-1) :
+            X_diff = function1_values[sorted_list[i]] - function1_values[sorted_list[i+1]]
+            Y_diff = function2_values[sorted_list[i]] - function2_values[sorted_list[i+1]]
+            d_i.append(math.sqrt(X_diff**2 + Y_diff**2))
+
+        average_euclidean_distance_obj_space = sum(d_i)/(pop_size-1)
+        mean_dev_d_i = [abs(x-average_euclidean_distance_obj_space) for x in d_i]
+        diversity_metric_obj_space = (d_l + d_f + sum(mean_dev_d_i))/(d_l + d_f + (pop_size-1)*average_euclidean_distance_obj_space)
+        print(diversity_metric_obj_space)
+        average_diversity_metric_objective_space += diversity_metric_obj_space
+
+        # print("FINAL SOLUTION : \n",population,"\n\n",max(population),"  ",min(population))
+
+    average_diversity_metric_objective_space = average_diversity_metric_objective_space/run_of_algorithm
+    # average_diversity_metric_data_space = average_diversity_metric_data_space/run_of_algorithm
+
+    print("Average Diversity Metric Obj Space:",average_diversity_metric_objective_space)
+    c = input("fdf")
     function1_values = [function1(population[i]) for i in range(0,pop_size)]
     function2_values = [function2(population[i]) for i in range(0,pop_size)]
 
@@ -308,12 +356,12 @@ if __name__ == '__main__' :
     plt.ylabel('(x-2)^2', fontsize=15)
     plt.scatter(function1_values, function2_values)
     plt.grid()
-    plt.savefig("O.png")
+    plt.show()
 
     f2 = plt.figure(figsize=(6.4,2))
     plt.plot(population,[0 for i in range(pop_size)],'x')
     plt.grid()
-    plt.savefig("D.png")
+    plt.show()
 
     # f3 = plt.figure(figsize=(12,7))
     # ax = f3.add_subplot(111)
